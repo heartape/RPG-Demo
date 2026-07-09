f4a5689e9c3b02e00368726f687a96c59e9cf6db
Alan Zhang <64122416+heartape@users.noreply.github.com>
2026-07-10 03:36:20 +0800

    创建task，并在状态树的idle状态中的task中添加wait

---
+++ Source/RPG/Private/AI/Task/STTask_Wait.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Task/STTask_Wait.h"
+
+#include "StateTreeExecutionContext.h"
+
+EStateTreeRunStatus FSTTask_Wait::EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	auto& [WaitTime, ElapsedTime] = Context.GetInstanceData(*this);
+	ElapsedTime = 0.f;
+	return EStateTreeRunStatus::Running;
+}
+
+EStateTreeRunStatus FSTTask_Wait::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	auto& [WaitTime, ElapsedTime] = Context.GetInstanceData(*this);
+	ElapsedTime += DeltaTime;
+	if (ElapsedTime >= WaitTime)
+	{
+		return EStateTreeRunStatus::Succeeded;
+	}
+
+	return EStateTreeRunStatus::Running;
+}
---
+++ Source/RPG/Public/AI/Task/STTask_Wait.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeTaskBase.h"
+#include "STTask_Wait.generated.h"
+
+USTRUCT()
+struct FRPGWaitTaskInstanceData
+{
+	GENERATED_BODY()
+
+	/** 等待时间（秒） */
+	UPROPERTY(EditAnywhere, Category="Parameter")
+	float WaitTime = 2.f;
+
+	/** 已等待时间 */
+	UPROPERTY()
+	float ElapsedTime = 0.f;
+};
+
+/**
+ * 
+ */
+USTRUCT(meta=(DisplayName="Wait"))
+struct RPG_API FSTTask_Wait : public FStateTreeTaskCommonBase
+{
+	GENERATED_BODY()
+
+	using FInstanceDataType = FRPGWaitTaskInstanceData;
+
+	virtual const UStruct* GetInstanceDataType() const override
+	{
+		return FInstanceDataType::StaticStruct();
+	}
+
+	virtual EStateTreeRunStatus EnterState(
+		FStateTreeExecutionContext& Context,
+		const FStateTreeTransitionResult& Transition) const override;
+
+	virtual EStateTreeRunStatus Tick(
+		FStateTreeExecutionContext& Context,
+		const float DeltaTime) const override;
+};
